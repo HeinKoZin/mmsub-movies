3fa2b3b2faa91889e766eab054b242ac6ae13efb
Hein Ko Zin <heinkozin4@gmail.com>
2021-10-22 03:11:13 +0630

    localStorageProvider is added for cache.

--- resources/js/index.tsx
+++ resources/js/index.tsx
@@ -2,15 +2,11 @@ import * as React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 import { BrowserRouter as Router } from "react-router-dom";
-import {
-    createTheme,
-    responsiveFontSizes,
-    Theme,
-    ThemeProvider as ThemeProviderOne,
-} from "@mui/material/styles";
+import { Theme, ThemeProvider as ThemeProviderOne } from "@mui/material/styles";
 import { ThemeProvider as ThemeProviderTwo } from "@mui/styles";
 import { SWRConfig } from "swr";
 import theme from "./components/Hooks/theme";
+import { localStorageProvider } from "./components/Hooks/localStorageProvider";
 
 declare module "@mui/styles" {
     interface DefaultTheme extends Theme {}
@@ -25,6 +21,7 @@ render(
                         refreshInterval: 3000,
                         fetcher: (resource, init) =>
                             fetch(resource, init).then((res) => res.json()),
+                        provider: localStorageProvider,
                     }}
                 >
                     <App />
